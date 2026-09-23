6d1dcec3f89ead086bd4f8771967b0c7918eefe5
geniushee <h1116jeon@gmail.com>
2026-09-24 01:49:22 +0900

    init

---
+++ transaction-producer/src/main/java/com/finstream/producer/MockTransactionProducer.java
@@ -0,0 +1,12 @@
+package com.finstream.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockTransactionProducer {
+    private static final Logger log = LoggerFactory.getLogger(MockTransactionProducer.class);
+
+    public static void main(String[] args) {
+        log.info("Starting MockTransactionProducer...");
+    }
+}
